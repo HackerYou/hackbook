45f6841fddc6e9b6bdd44dadba4257349c1cbc33
Ryan Brunner <ryan@influitive.com>
2014-04-28 17:14:59 -0400

    Finishing Omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
-    render json: request.env["omniauth.auth"]
+    auth = request.env['omniauth.auth']
+    @user = User.find_by(uid: auth.uid, provider: auth.provider)
+    if @user.nil?
+      @user = User.create!(uid: auth.uid, provider: auth.provider, name: auth.info.nickname)
+    end
+    sign_in_and_redirect @user, event: :authentication
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
-  validates :email, presence: true
+  #validates :email, presence: true
 
   has_many :posts
   has_many :friendships
